967acbddaec6f4f35b20f097991a1f76df284fc7
xavierhorwood <xaviergaming99900@gmail.com>
2022-07-30 21:31:47 -0700

    Update zone.cpp

--- src/engine/zone.cpp
+++ src/engine/zone.cpp
@@ -148,11 +148,8 @@ void Hunk_Print()
 void Memory_Init( void )
 {
 	MEM_ALLOC_CREDIT();
-#ifdef _X360
-	int nMaxBytes = 48*1024*1024;
-#else
-	int nMaxBytes = 64*1024*1024;
-#endif
+	//https://github.com/EnderZip/cool-source/commit/97e56d5b6161201e52b3a8f15725dbc6a8f36893
+	int nMaxBytes = 256*1024*1024;
 	const int nMinCommitBytes = 0x8000;
 #ifndef HUNK_USE_16MB_PAGE
 	const int nInitialCommit = 0x280000;
